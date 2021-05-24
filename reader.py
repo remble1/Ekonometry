a4cac3cc49e7422b41e3b17650fff6670e7b0a5c
pawel sadkowski <pawel.sadkowski.1c@gmail.com>
2021-05-24 18:30:33 +0200

    make a date reader

---
+++ reader.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_excel('dane.xlsx') # dateFrame from excel
+
+print(df.head())
--- window.py
+++ window.py
@@ -1,6 +1,17 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
+wb = Workbook()
+ws = wb.active
+
 
 wb2 = load_workbook('dane.xlsx')
+
 print(wb2.sheetnames)
 
+c = ws['A1']
+print(c)
 
+
+
+
+"""for row in range (1, wd.get_highest_row() + 1):
+    a = sheet['A' + str(row)].value"""
